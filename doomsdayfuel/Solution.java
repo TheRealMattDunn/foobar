540d0683e8fbecb349f6d15dd9dfdb420ac705cc
Matt Dunn <matthew.dunn@computacenter.com>
2022-10-21 16:00:39 +0100

    Final working solution for doomsday fuel challenge

--- doomsdayfuel/Solution.java
+++ doomsdayfuel/Solution.java
@@ -8,11 +8,11 @@ import java.util.stream.IntStream;
 public class Solution {
 
     private static class Fraction implements Cloneable {
-        private int numer;
-        private int denom;
+        private long numer;
+        private long denom;
 
-        public Fraction(int numer, int denom) {
-            int gcd = gcd(numer, denom);
+        public Fraction(long numer, long denom) {
+            long gcd = gcd(numer, denom);
 
             if (gcd == 0) {
                 System.err.println("here");
@@ -28,8 +28,8 @@ public class Solution {
         }
 
         public Fraction multiply(Fraction that) {
-            int numer = this.numer * that.numer;
-            int denom = this.denom * that.denom;
+            long numer = this.numer * that.numer;
+            long denom = this.denom * that.denom;
 
             return new Fraction(numer, denom);
         }
@@ -40,15 +40,15 @@ public class Solution {
         }
 
         public Fraction add(Fraction that) {
-            int numer = (this.numer * that.denom) + (that.numer * this.denom);
-            int denom = that.denom * this.denom;
+            long numer = (this.numer * that.denom) + (that.numer * this.denom);
+            long denom = that.denom * this.denom;
 
             return new Fraction(numer, denom);
         }
 
         public Fraction subtract(Fraction that) {
-            int numer = (this.numer * that.denom) - (that.numer * this.denom);
-            int denom = that.denom * this.denom;
+            long numer = (this.numer * that.denom) - (that.numer * this.denom);
+            long denom = that.denom * this.denom;
 
             return new Fraction(numer, denom);
         }
@@ -75,10 +75,10 @@ public class Solution {
 
         @Override
         public int hashCode() {
-            int hash = 7;
+            long hash = 7;
             hash = 31 * hash + this.numer;
             hash = 31 * hash + this.denom;
-            return hash;
+            return Long.hashCode(hash);
         }
 
         @Override
@@ -88,27 +88,22 @@ public class Solution {
     }
 
     private static Fraction[][] multiply(Fraction[][] a, Fraction[][] b) {
-        int aRows = a.length;
-        int aColumns = a[0].length;
-        int bRows = b.length;
-        int bColumns = b[0].length;
-
-        if (aColumns != bRows) {
-            // TODO change this
-            throw new IllegalArgumentException("A:Rows: " + aColumns + " did not match B:Columns " + bRows + ".");
+        if (a[0].length != b.length) {
+            throw new IllegalArgumentException(
+                    "Number of rows in a (" + a[0].length + ") must equal columns in b (" + b.length + ")");
         }
 
-        Fraction[][] c = new Fraction[aRows][bColumns];
+        Fraction[][] c = new Fraction[a.length][b[0].length];
 
-        for (int i = 0; i < aRows; i++) {
-            for (int j = 0; j < bColumns; j++) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < b[0].length; j++) {
                 c[i][j] = new Fraction(0, 1);
             }
         }
 
-        for (int i = 0; i < aRows; i++) {
-            for (int j = 0; j < bColumns; j++) {
-                for (int k = 0; k < aColumns; k++) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < b[0].length; j++) {
+                for (int k = 0; k < a[0].length; k++) {
                     c[i][j] = c[i][j].add(a[i][k].multiply(b[k][j]));
                 }
             }
@@ -118,7 +113,9 @@ public class Solution {
     }
 
     private static Fraction[][] subtract(Fraction[][] a, Fraction[][] b) {
-        // TODO - check matrices are same size
+        if (a.length != b.length || a[0].length != b[0].length) {
+            throw new IllegalArgumentException("a and b different sizes");
+        }
 
         Fraction[][] ans = new Fraction[a.length][a[0].length];
 
@@ -216,10 +213,6 @@ public class Solution {
         Fraction[][] inverseFundamental = subtract(identity, bMatrix);
         Fraction[][] fundamental = invert(inverseFundamental);
 
-        System.out.println(Arrays.deepToString(identity));
-        System.out.println(Arrays.deepToString(inverseFundamental));
-        System.out.println(Arrays.deepToString(fundamental));
-
         return fundamental;
     }
 
@@ -295,15 +288,15 @@ public class Solution {
     }
 
     // Greatest common divisor
-    private static int gcd(int n1, int n2) {
+    private static long gcd(long n1, long n2) {
         if (n2 == 0) {
             return n1;
         }
         return gcd(n2, n1 % n2);
     }
 
-    private static int lcm(int[] nums) {
-        int ans = nums[0];
+    private static long lcm(long[] nums) {
+        long ans = nums[0];
 
         for (int i = 1; i < nums.length; i++)
             ans = (nums[i] * ans) / gcd(nums[i], ans);
@@ -312,16 +305,6 @@ public class Solution {
     }
 
     public static int[] solution(int[][] m) {
-        // // Check for the all zeros case
-        // int[][] zeroMatrix = new int[m.length][m[0].length];
-
-        // if(Arrays.deepEquals(m, zeroMatrix)) {
-        // int[] ans = new int[m[0].length + 1];
-        // ans[0] = 1;
-        // ans[ans.length - 1] = 1;
-        // return ans;
-        // }
-
         Fraction[][] transMatrix = new Fraction[m.length][m[0].length];
 
         for (int i = 0; i < m.length; i++) {
@@ -343,7 +326,8 @@ public class Solution {
             }
         }
 
-        // Check for the case where all states are capturing
+        // Check for the case where all states are capturing - by comparing with the
+        // identity matrix
         Fraction[][] identityMatrix = new Fraction[transMatrix.length][transMatrix[0].length];
 
         for (int i = 0; i < transMatrix.length; i++) {
@@ -364,171 +348,56 @@ public class Solution {
         Fraction[][] a = getAMatrix(transMatrix);
         Fraction[][] solution = getSolutionMatrix(fundamental, a);
 
-        System.out.println(Arrays.deepToString(b));
-        System.out.println(Arrays.deepToString(fundamental));
-        System.out.println(Arrays.deepToString(a));
-        System.out.println(Arrays.deepToString(solution));
-
         Fraction[] absorbProbs = solution[0];
-        int[] denoms = new int[absorbProbs.length];
+        long[] denoms = new long[absorbProbs.length];
 
         for (int i = 0; i < absorbProbs.length; i++) {
             denoms[i] = absorbProbs[i].denom;
         }
 
-        int lcm = lcm(denoms);
-
+        long lcm = lcm(denoms);
         int[] ans = new int[absorbProbs.length + 1];
 
         for (int i = 0; i < absorbProbs.length; i++) {
             Fraction prob = absorbProbs[i];
-
-            ans[i] = prob.numer * lcm / prob.denom;
+            ans[i] = (int) (prob.numer * lcm / prob.denom);
         }
 
-        ans[ans.length - 1] = lcm;
-
+        ans[ans.length - 1] = (int) lcm;
         return ans;
     }
 
     public static void main(String[] args) {
-        // testFractionMultiply();
-        // testFractionAdd();
-        // testMatrixMultiply();
-        // testMatrixInvert();
-        // testGetAbsorbStates();
-        // testGetAbsorbProbs();
-        // testGcd();
-        // testLcm();
+        testFractionMultiply();
+        testFractionAdd();
+        testMatrixMultiply();
+        testMatrixInvert();
+        testGetAbsorbStates();
+        testGetAbsorbProbs();
+        testGcd();
+        testLcm();
         // testGetFundamentalMatrix();
 
-        // int[][] testCase1 = {
-        //         { 0, 2, 1, 0, 0 },
-        //         { 0, 0, 0, 3, 4 },
-        //         { 0, 0, 0, 0, 0 },
-        //         { 0, 0, 0, 0, 0 },
-        //         { 0, 0, 0, 0, 0 }
-        // };
-
-        // System.out.println(Arrays.toString(solution(testCase1)));
-
-        // int[][] testCase2 = {
-        //         { 0, 1, 0, 0, 0, 1 },
-        //         { 4, 0, 0, 3, 2, 0 },
-        //         { 0, 0, 0, 0, 0, 0 },
-        //         { 0, 0, 0, 0, 0, 0 },
-        //         { 0, 0, 0, 0, 0, 0 },
-        //         { 0, 0, 0, 0, 0, 0 }
-        // };
-
-        // System.out.println(Arrays.toString(solution(testCase2)));
-
-        // int[][] testCase3 = {
-        //         { 1, 2, 3, 0, 0, 0 },
-        //         { 4, 5, 6, 0, 0, 0 },
-        //         { 7, 8, 9, 1, 0, 0 },
-        //         { 0, 0, 0, 0, 1, 2 },
-        //         { 0, 0, 0, 0, 0, 0 },
-        //         { 0, 0, 0, 0, 0, 0 }
-        // };
-
-        // System.out.println(Arrays.toString(solution(testCase3)));
-
-        // int[][] testCase4 = {
-        //         { 0 }
-        // };
-
-        // System.out.println(Arrays.toString(solution(testCase4)));
-
-        int[][] testCase5 = {
-                { 0, 0, 12, 0, 15, 0, 0, 0, 1, 8 },
-                { 0, 0, 60, 0, 0, 7, 13, 0, 0, 0 },
-                { 0, 15, 0, 8, 7, 0, 0, 1, 9, 0 },
-                { 23, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
-                { 37, 35, 0, 0, 0, 0, 3, 21, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+        int[][] testCase1 = {
+                { 0, 2, 1, 0, 0 },
+                { 0, 0, 0, 3, 4 },
+                { 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0 }
         };
 
-        System.out.println(Arrays.toString(solution(testCase5)));
+        System.out.println(Arrays.toString(solution(testCase1)));
 
-        // int[][] testCase6 = {
-        //         { 0, 7, 0, 17, 0, 1, 0, 5, 0, 2 },
-        //         { 0, 0, 29, 0, 28, 0, 3, 0, 16, 0 },
-        //         { 0, 3, 0, 0, 0, 1, 0, 0, 0, 0 },
-        //         { 48, 0, 3, 0, 0, 0, 17, 0, 0, 0 },
-        //         { 0, 6, 0, 0, 0, 1, 0, 0, 0, 0 },
-        //         { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-        //         { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-        //         { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-        //         { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-        //         { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
-        // };
+        int[][] testCase2 = {
+                { 0, 1, 0, 0, 0, 1 },
+                { 4, 0, 0, 3, 2, 0 },
+                { 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0 }
+        };
 
-        // System.out.println(Arrays.toString(solution(testCase6)));
-
-        // int[][] testCase7 = {
-        //         { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
-        //         { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-        //         { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
-        //         { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-        //         { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
-        //         { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-        //         { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
-        //         { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-        //         { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
-        //         { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
-        // };
-
-        // System.out.println(Arrays.toString(solution(testCase7)));
-
-        // int[][] testCase8 = {
-        //         { 1, 1, 1, 0, 1, 0, 1, 0, 1, 0 },
-        //         { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-        //         { 1, 0, 1, 1, 1, 0, 1, 0, 1, 0 },
-        //         { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-        //         { 1, 0, 1, 0, 1, 1, 1, 0, 1, 0 },
-        //         { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-        //         { 1, 0, 1, 0, 1, 0, 1, 1, 1, 0 },
-        //         { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-        //         { 1, 0, 1, 0, 1, 0, 1, 0, 1, 1 },
-        //         { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
-        // };
-
-        // System.out.println(Arrays.toString(solution(testCase8)));
-
-        // int[][] testCase9 = {
-        //         { 0, 86, 61, 189, 0, 18, 12, 33, 66, 39 },
-        //         { 0, 0, 2, 0, 0, 1, 0, 0, 0, 0 },
-        //         { 15, 187, 0, 0, 18, 23, 0, 0, 0, 0 },
-        //         { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-        //         { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-        //         { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-        //         { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-        //         { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-        //         { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-        //         { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
-        // };
-
-        // System.out.println(Arrays.toString(solution(testCase9)));
-
-        // int[][] testCase10 = {
-        //         { 0, 0, 0, 0, 3, 5, 0, 0, 0, 2 },
-        //         { 0, 0, 4, 0, 0, 0, 1, 0, 0, 0 },
-        //         { 0, 0, 0, 4, 4, 0, 0, 0, 1, 1 },
-        //         { 13, 0, 0, 0, 0, 0, 2, 0, 0, 0 },
-        //         { 0, 1, 8, 7, 0, 0, 0, 1, 3, 0 },
-        //         { 1, 7, 0, 0, 0, 0, 0, 2, 0, 0 },
-        //         { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-        //         { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-        //         { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-        //         { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
-        // };
-
-        // System.out.println(Arrays.toString(solution(testCase10)));
+        System.out.println(Arrays.toString(solution(testCase2)));
     }
 
     // Rought and ready unit tests
@@ -617,11 +486,7 @@ public class Solution {
     }
 
     private static void testGetFundamentalMatrix() {
-        // { 0, 2, 1, 0, 0 },
-        // { 0, 0, 0, 3, 4 },
-        // { 0, 0, 0, 0, 0 },
-        // { 0, 0, 0, 0, 0 },
-        // { 0, 0, 0, 0, 0 }
+        // WARNING - this test currently fails
 
         Fraction[][] matrix = {
                 { new Fraction(0, 1), new Fraction(2, 3), new Fraction(1, 3), new Fraction(0, 1), new Fraction(0, 1) },
@@ -639,9 +504,9 @@ public class Solution {
 
         System.out.println(Arrays.deepToString(getFundamentalMatrix(matrix)));
 
-        // if(! Arrays.deepEquals(getFundamentalMatrix(matrix), ans)) {
-        // throw new AssertionError();
-        // }
+        if (!Arrays.deepEquals(getFundamentalMatrix(matrix), ans)) {
+            throw new AssertionError();
+        }
     }
 
     private static void testGetAbsorbProbs() {
@@ -664,8 +529,8 @@ public class Solution {
     }
 
     private static void testGcd() {
-        int int1 = 63;
-        int int2 = 117;
+        long int1 = 63;
+        long int2 = 117;
 
         if (gcd(int1, int2) != 9) {
             throw new AssertionError();
@@ -673,7 +538,7 @@ public class Solution {
     }
 
     private static void testLcm() {
-        int[] nums = { 12, 15, 75 };
+        long[] nums = { 12, 15, 75 };
 
         if (lcm(nums) != 300) {
             throw new AssertionError();
